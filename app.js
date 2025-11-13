854dcd3d614b6a1a737a2b67c203f7e513fe48ed
SmartandWire <jojojuls2111@gmail.com>
2025-11-13 14:08:08 +0000

    app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     console.log(entry)
     if (entry.isIntersecting) { 
-      entry.traget.classList.add('show')
+      entry.target.classList.add('show')
     } else {
       entry.target.classList.remove('show');
     }
